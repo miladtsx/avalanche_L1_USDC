3dec8767b19e3f0593a2dc4d64d3a20bc605b55e
MehdizadeMilad <milad.mehdizade1394@gmail.com>
2024-09-30 22:01:22 +0000

    fix(theme): header.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,12 +16,12 @@ const Footer: React.FC = () => {
                 href="https://github.com/miladtsx/avalanche_custom_blockchain"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-gray-200 hover:text-gray-800"
+                className="text-gray-200 hover:text-gray-500"
             >
                 <FontAwesomeIcon icon={faGithub} className="h-5 w-5 inline-block" />
             </a>
 
-            <p className="text-xs text-white pb-8">
+            <p className="text-xs text-white pb-8 pt-4">
                 @2024, with curiosity and love.
             </p >
         </footer >
